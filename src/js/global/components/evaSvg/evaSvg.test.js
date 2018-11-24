c202ee853b3e82e344a1fdfb3652dbe75b6ea97c
Witold Budzioch <witooostudio@gmail.com>
2018-11-24 01:39:28 +0100

    Fix typo

--- src/js/global/components/evaSvg/evaSvg.test.js
+++ src/js/global/components/evaSvg/evaSvg.test.js
@@ -2,12 +2,12 @@ import React from 'react'
 import {render, mount} from 'enzyme';
 import EvaSvg from './';
 
-describe('(Component) <RegisterForm />', () => {
-	test ('(Component) <EvaSvg /> should render svg element', () => {
+describe('(Component) <EvaSvg />', () => {
+	test ('Should render svg element', () => {
 		const wrapper = render(<EvaSvg icon={'checkmark-circle-outline'} />);
 		expect(wrapper.find('svg').length).toBe(1);
 	});
-	test ('(Component) <EvaSvg /> should throw an error if icon prop is not supplied', () => {
+	test ('Should throw an error if icon prop is not supplied', () => {
 		let error;
 		try {
 			shallow(<EvaSvg />)
